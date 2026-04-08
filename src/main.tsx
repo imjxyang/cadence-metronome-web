2808b93e2f71e12f95a15fdd5edecd30e86c044e
Jiaxi Yang <jxyang.dev@gmail.com>
2026-04-08 22:45:40 +0800

    Init

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import { StrictMode } from "react";
+
+import App from "./App.tsx";
+
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("Root element not found");
+}
+createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
